8e73563868f70d11849b693f0dff4634ccdb625b
rj <rjgilo2799@gmail.com>
2022-02-26 00:56:04 -0500

    added newpage

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -1,5 +1,6 @@
 """This test the homepage"""
 
+
 def test_request_main_menu_links(client):
     """This makes the index page"""
     response = client.get("/")
@@ -9,6 +10,8 @@ def test_request_main_menu_links(client):
     assert b'<a class="nav-link" href="/page2">Page 2</a>' in response.data
     assert b'<a class="nav-link" href="/page3">Page 3</a>' in response.data
     assert b'<a class="nav-link" href="/page4">Page 4</a>' in response.data
+    assert b'<a class="nav-link" href="/page4">Page 6</a>' in response.data
+
 
 def test_request_index(client):
     """This makes the index page"""
@@ -16,11 +19,13 @@ def test_request_index(client):
     assert response.status_code == 200
     assert b"Index Page" in response.data
 
+
 def test_request_about(client):
     """This makes the index page"""
     response = client.get("/about")
     assert response.status_code == 200
     assert b"About Page" in response.data
+
 
 def test_request_page1(client):
     """This makes the index page"""
@@ -28,11 +33,13 @@ def test_request_page1(client):
     assert response.status_code == 200
     assert b"Page 1" in response.data
 
+
 def test_request_page2(client):
     """This makes the index page"""
     response = client.get("/page2")
     assert response.status_code == 200
     assert b"Page 2" in response.data
+
 
 def test_request_page3(client):
     """This makes the index page"""
@@ -40,14 +47,22 @@ def test_request_page3(client):
     assert response.status_code == 200
     assert b"Page 3" in response.data
 
+
 def test_request_page4(client):
     """This makes the index page"""
     response = client.get("/page4")
     assert response.status_code == 200
     assert b"Page 4" in response.data
 
+
+def test_request_page6(client):
+    """This makes the index page"""
+    response = client.get("/page6")
+    assert response.status_code == 200
+    assert b"Page 4" in response.data
+
+
 def test_request_page_not_found(client):
     """This makes the index page"""
     response = client.get("/page5")
     assert response.status_code == 404
-
