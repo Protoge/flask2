d72fe7fb773dcda756f055a4316c33ad8708a6f3
rj <rjgilo2799@gmail.com>
2022-03-01 10:24:46 -0500

    edit added page 4 Continuous Integration and Deployment

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -9,6 +9,7 @@ def test_request_main_menu_links(client):
     assert b'<a class="nav-link" href="/page1">Python/Flask</a>' in response.data
     assert b'<a class="nav-link" href="/page2">Docker</a>' in response.data
     assert b'<a class="nav-link" href="/page3">Git</a>' in response.data
+    assert b'<a class="nav-link" href="/page4">Continuous Integration and Deployment</a>' in response.data
 
 
 def test_request_index(client):
@@ -44,6 +45,12 @@ def test_request_page3(client):
     response = client.get("/page3")
     assert response.status_code == 200
     assert b"Page 3" in response.data
+
+def test_request_page4(client):
+    """This makes the index page"""
+    response = client.get("/page4")
+    assert response.status_code == 200
+    assert b"Page 4" in response.data
 
 def test_request_page_not_found(client):
     """This makes the index page"""
