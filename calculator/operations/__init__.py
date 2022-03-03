d7745da41fcdae9f36445ad7d6f69a5e79624958
rj <rjgilo2799@gmail.com>
2022-03-03 01:01:25 -0500

    latest calc update

---
+++ calculator/operations/__init__.py
@@ -0,0 +1,25 @@
+""" This is the Calculator Class"""
+
+
+class Addition:
+
+    def add(value_1, value_2):
+        """ This is the add method"""
+
+        return value_1 + value_2
+
+
+class Subtraction:
+
+    def subtract(value_1, value_2):
+        """ This is the subtract method"""
+
+        return value_1 - value_2
+
+
+class Multiplication:
+
+    def multiply(value_1, value_2):
+        """ This is the multiply method"""
+
+        return value_1 * value_2
